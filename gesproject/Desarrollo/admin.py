65f69cd01554f75380a4ebf95849bc37bd4f3c8b
paulo-arias <61350999+paulo-arias@users.noreply.github.com>
2020-03-22 19:43:05 -0400

    Update admin.py

--- gesproject/Desarrollo/admin.py
+++ gesproject/Desarrollo/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
-from .models import Desarrollo
+from .models import Tarea
+from .models import Proyecto
+
 # Register your models here.
-@admin.register(Desarrollo)
+@admin.register(Tarea)
+
 # Se crea una clase que extienda del admin.ModelAdmin
-class DesarrolloAdmin(admin.ModelAdmin):
-    list_display = (id,'tarea','proyecto')
+class TareaAdmin(admin.ModelAdmin):
+    list_display = (id,'version','prioridad','estado','descripcion','observacion')
+
+@admin.register(Proyecto)
+class ProyectoAdmin(admin.ModelAdmin):
+    list_display = (id,'Nombre','Estado')
