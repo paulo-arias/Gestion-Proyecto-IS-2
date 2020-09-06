4ea61b99ad80ef9f4268632d325403f52b8d227d
NelsonSMG <32785313+NelsonSMG@users.noreply.github.com>
2020-09-06 19:34:08 -0400

    Update models.py

--- gesproject/Desarrollo/models.py
+++ gesproject/Desarrollo/models.py
@@ -40,8 +40,38 @@ class Tarea(models.Model):
 
     #linea base
     #linea_base = models.ForeignKey('Configuracion.LineaBase', null=True, blank=True, help_text='Linea Base a la que pertence', on_delete=models.SET_NULL)  # , on_delete=models.CASCADE
+    
+    #variables para guardar los valores iniciales
+    og_version = None
+    og_prioridad = None
+    og_estado = None
+    og_descripcion = None
+    og_observacion = None
+    og_tareapadre = None
+    og_proyecto = None
+    og_request = None
 
-
+    def __init__(self, *args, **kwargs):
+        super(Tarea, self).__init__(*args, **kwargs)
+        #guardamos los valores inicales
+        self.og_version = self.version
+        self.og_prioridad = self.prioridad
+        self.og_estado = self.estado
+        self.og_descripcion = self.descripcion
+        self.og_observacion = self.observacion
+        self.og_tareapadre = self.version
+        self.og_proyecto = self.version
+        
+    #def save(self, *args, **kwargs):
+    #   if self.linea_base:
+    #        if (self.og_version != self.version) or (self.og_prioridad != self.prioridad) or (self.og_estado != self.estado) or (self.og_descripcion != self.descripcion) or (self.og_observacion != self.observacion) or (self.og_tareapadre != self.version) or (self.og_proyecto != self.version):
+    #            #messages.warning('POST', 'Tarea en Linea base no!')
+    #            raise Exception("Tarea en Linea base no puede ser modificada")
+    #        else:
+    #            super(Tarea, self).save(*args, **kwargs)
+    #    else:
+    #        super(Tarea, self).save(*args, **kwargs)
+    
     def __str__(self):
         # Al hacer el get de la tarea despliega su descripción
         return 'Nombre: ' + self.descripcion + ', Estado: '+ self.estado + ', Version: ' + self.version
