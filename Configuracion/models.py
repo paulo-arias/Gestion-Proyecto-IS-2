faeef1c5d90567c7e42eb108ea943d475f285029
NelsonSMG <32785313+NelsonSMG@users.noreply.github.com>
2020-10-11 21:13:34 -0300

    Add files via upload

---
+++ Configuracion/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+
+class Configuracion(models.Model):
+    nombre = models.CharField(max_length=255)
+    apellido = models.CharField(max_length=255)
+
+
+class LineaBase(models.Model):
+    Codigo = models.IntegerField(primary_key=True)  # si le quiero poner
+    Nombre = models.CharField(max_length=255)
+
+    estados_lb = (
+        ('i', 'Iniciado'),
+        ('p', 'Pendiente'),
+        ('f', 'Finalizado')
+    )
+
+    Estado = models.CharField(max_length=1, choices=estados_lb, blank=True, default='p', help_text='Estados de la Tarea')
